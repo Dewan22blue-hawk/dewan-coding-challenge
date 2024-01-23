0814f1091c3361a5e0706e265268de5719b73af0
Petani sawah <dennyirawan721@smk.belajar.id>
2024-01-23 15:13:48 +0700

    Firat Day of 1/366

---
+++ 1-grow.js
@@ -0,0 +1,28 @@
+// function grow(x) {
+//     let result = x[0];
+//     for(let i = 1;i<x.length;i++){
+//         // result = result * x[i];
+//         result  *= x[i];
+//     }
+//     return result;
+// }
+
+// menggunakan hair order function reduce
+// function grow(x) {
+// const result = x.reduce((acc, curr)=>{
+//     return acc*curr;
+// }, 1);
+// return result
+// }
+
+
+// // meringkas atau one liner code
+// function grow(x) {
+// return x.reduce((acc, curr)=>acc*curr, 1);
+
+// }
+
+// menggunakan function expression
+const grow = (x)=> x.reduce((acc, curr)=>acc*curr, 1);
+
+console.log(grow([2, 2, 2, 2, 2, 2]))

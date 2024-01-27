48d91be1c07678cd28d22108b3d82d6489a52847
Dewan22blue-hawk <dennyirawan170204@gmail.com>
2024-01-27 11:15:58 +0700

    day 4 smash join

---
+++ 4-sentence-smash.js
@@ -0,0 +1,6 @@
+// function smash(words) {
+//   return words.join(" ");
+// }
+
+const smash = (words) => words.join(" ");
+console.log(smash(["hello", "amazing", "world"]), "hello amazing world");
